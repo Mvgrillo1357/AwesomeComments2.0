df23a7d510fcf2f8969a017979a3df1cf0a137fc
Michael Grillo <58634459+Mvgrillo1357@users.noreply.github.com>
2022-06-01 10:50:26 -0400

    update ml_models.py

--- models/ml_models.py
+++ models/ml_models.py
@@ -3,8 +3,8 @@ import pandas as pd
 df=pd.read_csv("E:/summer22/individual_project/AwesomeComments2.0/models/cyberbullying_tweets_full.csv")
 #print(df.head()) good this is the correct pathing
 import pickle
-#to save the dataframe, df to 123.pkl
+#to save the dataframe, df to df.pkl
 df.to_pickle("E:/summer22/individual_project/AwesomeComments2.0/models/df.pkl")    
-#to load 123.pkl back to the dataframe df
+#to load 123.pkl back to the dataframe tweet_df
 tweet_df = pd.read_pickle("E:/summer22/individual_project/AwesomeComments2.0/models/df.pkl") 
 print(tweet_df.head(20))
