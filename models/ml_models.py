d1a52e5772b0a67fd93a84f928a62f60747cd677
Michael Grillo <58634459+Mvgrillo1357@users.noreply.github.com>
2022-06-01 10:23:36 -0400

    csv to df to pickle
    using absolute pathing this needs to change

--- models/ml_models.py
+++ models/ml_models.py
@@ -1,8 +1,10 @@
 ### Building csv for Training ###
 import pandas as pd
-df=pd.read_csv('AwesomeComments2.0/models/cyberbullying_tweets_full.csv')
+df=pd.read_csv("E:/summer22/individual_project/AwesomeComments2.0/models/cyberbullying_tweets_full.csv")
+#print(df.head()) good this is the correct pathing
+import pickle
 #to save the dataframe, df to 123.pkl
-df.to_pickle('/models/cyberbullying_tweets_full.csv/df.pkl')    
+df.to_pickle("E:/summer22/individual_project/AwesomeComments2.0/models/df.pkl")    
 #to load 123.pkl back to the dataframe df
-tweet_df = pd.read_pickle('/Drive Path/df.pkl') 
+tweet_df = pd.read_pickle("E:/summer22/individual_project/AwesomeComments2.0/models/df.pkl") 
 print(tweet_df.head(20))
